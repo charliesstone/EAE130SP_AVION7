96f06bb685d783c6852954a7f64387ca166c5292
charliesstone <cbstone@ucdavis.edu>
2026-02-02 18:06:56 -0800

    sizing code rev2

--- Conceptual Sizing/constraint_diag_master.py
+++ Conceptual Sizing/constraint_diag_master.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 ## ------ Team 7 Avion: Mostafa Hashem, Charlie Stone, Jose Hernandez Negrete, ------ ##
 ## ------ Valeria Cecena, Quinn Kennerly, Lena Pattamadilok ------ ##
 
-#region input parameters
+#region inputs
 g = 32.174 #ft/s^2, gravitational acceleration
 tar_togw = 50,000 #lbf, target gross take off weight
 S_i = 600 #ft^2, initial wing area assumption including strakes (LERX)
@@ -15,6 +15,19 @@ C_D0 = 0.01755 #initial zero-lift drag estimation, from OpenVSP, see:
 e = 0.8 #oswald efficiency factor for fighters, from raymond textbook
 nz_tar = 7.0 * g #ft/s^2, load factor, minimum targeted
 nz_idl = 8.0 * g #ft/s^2, load factor, ideal targeted
+wingload = np.linspace(0, 200, 500)
+thrustload = np.linspace(0, 2.5, 500)
+#endregion
+
+#region stall
+Vstall_L = 220 #ft/s (130 KIAS), stall speed for landing (based on F/A-18E/F)
+Vstall_C = 195 #ft/s (115 KIAS), stall speed for cruise (based on F/A-18E/F)
+Vstall_T = 205 #ft/s (120 KIAS), stall speed for takeoff (based on F/A-18E/F)
+rho_30k = 8.91E-4 #slugs/ft^3, atmospheric density at 30k ft
+rho_SL = 23.77E-4 #slugs/ft^3, atmospheric density at sea level
+CLmax_L = 2.0
+CLmax_C = 1.5
+CLmax_T = 1.7
 
 
 
