a39b49de5919384c7d62652b39843de9000cf8b8
Lena Pattamadilok <lpattamadilok@ucdavis.edu>
2026-04-02 19:33:04 -0700

    added b1 folder and fuselage weight

---
+++ Assignments/B1/fus_weight.py
@@ -0,0 +1,12 @@
+def fuselage_weight(Kdwf, Wdg, Nz, L, D, W):
+    return 0.499 * Kdwf * (Wdg ** 0.35) * (Nz ** 0.25) * (L ** 0.5) * (D ** 0.849) * (W ** 0.685)
+
+Kdwf = 1.0
+Wdg = 36700.0
+Nz = 12.0
+L = 40.3604
+D = 5.5
+W = 5.0
+
+W_fuselage = fuselage_weight(Kdwf, Wdg, Nz, L, D, W)
+print(f"Fuselage structural weight = {W_fuselage:.2f} lb")
