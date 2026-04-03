acc4a6ff6d4be3a92c2388d8d06795b63bcf8ac5
Lena Pattamadilok <lpattamadilok@ucdavis.edu>
2026-04-02 19:38:19 -0700

    added comments

--- Assignments/B1/fus_weight.py
+++ Assignments/B1/fus_weight.py
@@ -4,6 +4,8 @@ def fuselage_weight(Kdwf, Wdg, Nz, L, D, W):
 Kdwf = 1.0
 Wdg = 36700.0
 Nz = 12.0
+# dimensions from a5_geom1.vsp3
+# values may change once we get charlie's most recent vsp file 
 L = 40.3604
 D = 5.5
 W = 5.0
