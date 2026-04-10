9b1331c60a4803aa7c047ac25fe7610dd7e9711d
vacecena <vacecena@ucdavis.edu>
2026-04-09 20:45:49 -0700

    Update weightval.py

--- Assignments/B1/weightval.py
+++ Assignments/B1/weightval.py
@@ -1,3 +1,17 @@
+import math
+
+# ============================================
+# Control System Weight (Raymer)
+# ============================================
+
+# Inputs
+M = 2.0                  # Mach number
+N_en = 1.0               # number of engines
+N_c = 1.0                # number of crew
+L_ec = 15.45             # ft (engine to cockpit distance)
+N_s = 4.0                # number of control systems
+
+# Control surface areas (ft^2)
 S_csw = 105.33
 S_vt = 281.28
 Sr_over_Svt = 0.045
